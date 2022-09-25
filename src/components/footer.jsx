816a90f3e03895a351247b73c76f36cd34b2eae8
Nuthan Prasad <nuthan1159@gmail.com>
2022-09-25 09:25:31 +0530

    added react icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {  FaTwitterSquare } from 'react-icons/fa';
+import {  FaGithubSquare, FaTwitterSquare } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer>
-       <h2> made by <a href="https://twitter.com/nuthan_2x">nuthan_2x  </a>  <a href="https://twitter.com/nuthan_2x" className='tweet'><FaTwitterSquare />  </a>&nbsp; inspired from <a href="https://www.youtube.com/c/ArturChmaro">ArturChmaro</a></h2>
+       <h2> made by <a href="https://twitter.com/nuthan_2x">nuthan_2x  </a>  <a href="https://twitter.com/nuthan_2x" className='icon'><FaTwitterSquare />  </a><a href="https://github.com/nuthan2x/Sign-Verify-digital-signatures" className='icon'><FaGithubSquare /></a>&nbsp; &nbsp; inspired from <a href="https://www.youtube.com/c/ArturChmaro">ArturChmaro</a></h2>
     </footer>
   )
 }
